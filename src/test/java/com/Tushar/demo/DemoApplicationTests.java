f458bc285231acd988c3408f1c249c75360f4de9
Tusharc5656 <tushar2004sk@gmail.com>
2026-07-13 11:16:04 +0530

    Student class

---
+++ src/test/java/com/Tushar/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tushar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
