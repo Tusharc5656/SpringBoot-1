11c3eae5111b61684073e569a5b0ea347439ce9f
Tusharc5656 <tushar2004sk@gmail.com>
2026-07-15 10:46:48 +0530

    postWithStudent

--- src/main/java/com/Tushar/demo/StudentServer/Student.java
+++ src/main/java/com/Tushar/demo/StudentServer/Student.java
@@ -1,9 +1,40 @@
 package com.Tushar.demo.StudentServer;
-@Entity
-public class Student {
 
-    long Id;
+public class Student {
+    int id;
     String name;
     int age;
     String department;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
