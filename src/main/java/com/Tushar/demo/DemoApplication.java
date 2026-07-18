9bcd538de1f7b02accf03939222941579c02a969
Tusharc5656 <tushar2004sk@gmail.com>
2026-07-19 02:17:43 +0530

    frontend & backend

--- src/main/java/com/Tushar/demo/DemoApplication.java
+++ src/main/java/com/Tushar/demo/DemoApplication.java
@@ -4,6 +4,10 @@ import com.Tushar.demo.Dependecyinjection.OrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,6 +24,19 @@ public class DemoApplication {
 	/*	OrderService orderService = context.getBean(OrderService.class);
 
 		orderService.placeOrder();*/
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
 	}
 
 }
